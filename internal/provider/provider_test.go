b15d0bdd296cbe8701865dbc21e03469c61476fe
xorps <758027+xorps@users.noreply.github.com>
2025-02-23 12:37:15 -0700

    new resource

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -22,7 +22,7 @@ var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServe
 // It allows for testing assertions on data returned by an ephemeral resource during Open.
 // The echoprovider is used to arrange tests by echoing ephemeral data into the Terraform state.
 // This lets the data be referenced in test assertions with state checks.
-var testAccProtoV6ProviderFactoriesWithEcho = map[string]func() (tfprotov6.ProviderServer, error){
+var testAccProtoV6ProviderFactoriesWithEcho = map[string]func() (tfprotov6.ProviderServer, error){ //nolint:unused
 	"scaffolding": providerserver.NewProtocol6WithError(New("test")()),
 	"echo":        echoprovider.NewProviderServer(),
 }
