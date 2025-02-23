46823df4f990e58b78bd4134b0d02f55d8bdbc72
xorps <758027+xorps@users.noreply.github.com>
2025-02-23 13:13:43 -0700

    new resource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -5,96 +5,110 @@ package provider
 
 import (
 	"context"
-	"net/http"
+	"fmt"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"github.com/hashicorp/go-tfe"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
-	"github.com/hashicorp/terraform-plugin-framework/ephemeral"
-	"github.com/hashicorp/terraform-plugin-framework/function"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-// Ensure ScaffoldingProvider satisfies various provider interfaces.
-var _ provider.Provider = &ScaffoldingProvider{}
-var _ provider.ProviderWithFunctions = &ScaffoldingProvider{}
-var _ provider.ProviderWithEphemeralResources = &ScaffoldingProvider{}
+// Ensure TfSyncProvider satisfies various provider interfaces.
+var _ provider.Provider = &TfSyncProvider{}
 
-// ScaffoldingProvider defines the provider implementation.
-type ScaffoldingProvider struct {
+// TfSyncProvider defines the provider implementation.
+type TfSyncProvider struct {
 	// version is set to the provider version on release, "dev" when the
 	// provider is built and ran locally, and "test" when running acceptance
 	// testing.
 	version string
 }
 
-// ScaffoldingProviderModel describes the provider data model.
-type ScaffoldingProviderModel struct {
-	Endpoint types.String `tfsdk:"endpoint"`
+// TfSyncProviderModel describes the provider data model.
+type TfSyncProviderModel struct {
+	RoleArn              types.String `tfsdk:"role_arn"`
+	WebIdentityTokenFile types.String `tfsdk:"web_identity_token_file"`
 }
 
-func (p *ScaffoldingProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
-	resp.TypeName = "scaffolding"
+func (p *TfSyncProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "tfsync"
 	resp.Version = p.version
 }
 
-func (p *ScaffoldingProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+func (p *TfSyncProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
-			"endpoint": schema.StringAttribute{
-				MarkdownDescription: "Example provider attribute",
-				Optional:            true,
+			"role_arn": schema.StringAttribute{
+				MarkdownDescription: "role arn to assume for s3 client",
+				Required:            true,
+			},
+			"web_identity_token_file": schema.StringAttribute{
+				MarkdownDescription: "path to web identity token file for s3 client",
+				Required:            true,
 			},
 		},
 	}
 }
 
-func (p *ScaffoldingProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
-	var data ScaffoldingProviderModel
+type ResourceConfigureData struct {
+	tfeClient *tfe.Client
+	s3Client  *s3.Client
+}
 
+func NewResourceConfigureData(tfeClient *tfe.Client, s3Client *s3.Client) *ResourceConfigureData {
+	return &ResourceConfigureData{tfeClient: tfeClient, s3Client: s3Client}
+}
+
+func (p *TfSyncProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	var data TfSyncProviderModel
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Configuration values are now available.
-	// if data.Endpoint.IsNull() { /* ... */ }
+	tfeClient, err := tfe.NewClient(tfe.DefaultConfig())
+	if err != nil {
+		resp.Diagnostics.AddError("tfe client", fmt.Sprintf("failed to create tfe client: %s", err))
+		return
+	}
 
-	// Example client configuration for data sources and resources
-	client := http.DefaultClient
-	resp.DataSourceData = client
-	resp.ResourceData = client
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		resp.Diagnostics.AddError("aws client", fmt.Sprintf("failed to load AWS configuration: %s", err))
+		return
+	}
+
+	stsClient := sts.NewFromConfig(cfg)
+	webIdentityProvider := stscreds.NewWebIdentityRoleProvider(stsClient, data.RoleArn.String(), stscreds.IdentityTokenFile(data.WebIdentityTokenFile.String()))
+	cfg.Credentials = aws.NewCredentialsCache(webIdentityProvider)
+	s3Client := s3.NewFromConfig(cfg)
+
+	cd := NewResourceConfigureData(tfeClient, s3Client)
+
+	resp.DataSourceData = cd
+	resp.ResourceData = cd
 }
 
-func (p *ScaffoldingProvider) Resources(ctx context.Context) []func() resource.Resource {
+func (p *TfSyncProvider) Resources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
-		NewExampleResource,
+		NewS3ObjectResource,
 	}
 }
 
-func (p *ScaffoldingProvider) EphemeralResources(ctx context.Context) []func() ephemeral.EphemeralResource {
-	return []func() ephemeral.EphemeralResource{
-		NewExampleEphemeralResource,
-	}
-}
-
-func (p *ScaffoldingProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
-	return []func() datasource.DataSource{
-		NewExampleDataSource,
-	}
-}
-
-func (p *ScaffoldingProvider) Functions(ctx context.Context) []func() function.Function {
-	return []func() function.Function{
-		NewExampleFunction,
-	}
+func (p *TfSyncProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return nil
 }
 
 func New(version string) func() provider.Provider {
 	return func() provider.Provider {
-		return &ScaffoldingProvider{
+		return &TfSyncProvider{
 			version: version,
 		}
 	}
---
+++ internal/provider/s3_object_resource.go
@@ -0,0 +1,311 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/hashicorp/go-tfe"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ resource.Resource = &S3ObjectResource{}
+var _ resource.ResourceWithImportState = &S3ObjectResource{}
+
+func NewS3ObjectResource() resource.Resource {
+	return &S3ObjectResource{}
+}
+
+type S3ObjectResource struct {
+	tfeClient *tfe.Client
+	s3Client  *s3.Client
+}
+
+type S3ObjectResourceModel struct {
+	Id                   types.String `tfsdk:"id"`
+	WorkspaceId          types.String `tfsdk:"workspace_id"`
+	Bucket               types.String `tfsdk:"bucket"`
+	Key                  types.String `tfsdk:"key"`
+	StateContentsSha256  types.String `tfsdk:"state_contents_sha256"`
+	BucketContentsSha256 types.String `tfsdk:"bucket_contents_sha256"`
+}
+
+func (r *S3ObjectResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_s3_object"
+}
+
+func (r *S3ObjectResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Resource to sync tf-state to an s3 object",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Example identifier",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"workspace_id": schema.StringAttribute{
+				MarkdownDescription: "terraform workspace id",
+				Required:            true,
+			},
+			"bucket": schema.StringAttribute{
+				MarkdownDescription: "s3 bucket",
+				Required:            true,
+			},
+			"key": schema.StringAttribute{
+				MarkdownDescription: "s3 bucket key",
+				Required:            true,
+			},
+			"state_contents_sha256": schema.StringAttribute{
+				MarkdownDescription: "sha256 sum of tf state",
+				Computed:            true,
+			},
+			"bucket_contents_sha256": schema.StringAttribute{
+				MarkdownDescription: "sha256 sum of s3 bucket object contents",
+				Computed:            true,
+			},
+		},
+	}
+}
+
+func (r *S3ObjectResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		resp.Diagnostics.AddError("configure", "nil provider data")
+		return
+	}
+
+	data, ok := req.ProviderData.(*ResourceConfigureData)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *ResourceConfigureData, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+
+	r.tfeClient = data.tfeClient
+	r.s3Client = data.s3Client
+}
+
+func (r *S3ObjectResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	resp.Diagnostics.Append(validateS3ObjectResource(r)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var data S3ObjectResourceModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	state, d := getStateFile(ctx, r.tfeClient, data.WorkspaceId.String())
+	resp.Diagnostics.Append(d...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(putS3ObjectContents(ctx, r.s3Client, data.Bucket.String(), data.Key.String(), state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	data.Id = newS3ObjectResourceID(&data)
+	data.StateContentsSha256 = sha256Contents(state)
+	data.BucketContentsSha256 = sha256Contents(state)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *S3ObjectResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	resp.Diagnostics.Append(validateS3ObjectResource(r)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var data S3ObjectResourceModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	state, d := getStateFile(ctx, r.tfeClient, data.WorkspaceId.String())
+	resp.Diagnostics.Append(d...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	contents, d := getS3ObjectContents(ctx, r.s3Client, data.Bucket.String(), data.Key.String())
+	resp.Diagnostics.Append(d...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	data.Id = newS3ObjectResourceID(&data)
+	data.StateContentsSha256 = sha256Contents(state)
+	data.BucketContentsSha256 = sha256Contents(contents)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *S3ObjectResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	resp.Diagnostics.Append(validateS3ObjectResource(r)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var data S3ObjectResourceModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	state, d := getStateFile(ctx, r.tfeClient, data.WorkspaceId.String())
+	resp.Diagnostics.Append(d...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(putS3ObjectContents(ctx, r.s3Client, data.Bucket.String(), data.Key.String(), state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	data.StateContentsSha256 = sha256Contents(state)
+	data.BucketContentsSha256 = sha256Contents(state)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *S3ObjectResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	resp.Diagnostics.Append(validateS3ObjectResource(r)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var data S3ObjectResourceModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(deleteS3Object(ctx, r.s3Client, data.Bucket.String(), data.Key.String())...)
+}
+
+func (r *S3ObjectResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resp.Diagnostics.Append(validateS3ObjectResource(r)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
+
+func sha256Contents(contents []byte) basetypes.StringValue {
+	hash := sha256.Sum256(contents)
+	hashString := hex.EncodeToString(hash[:])
+	return types.StringValue(hashString)
+}
+
+func newS3ObjectResourceID(data *S3ObjectResourceModel) basetypes.StringValue {
+	return types.StringValue(fmt.Sprintf("%s/%s/%s", data.WorkspaceId.String(), data.Bucket.String(), data.Key.String()))
+}
+
+func getStateFile(ctx context.Context, client *tfe.Client, workspaceId string) (state []byte, diag diag.Diagnostics) {
+	ver, err := client.StateVersions.ReadCurrent(ctx, workspaceId)
+	if err != nil {
+		diag.AddError("tfe client", fmt.Sprintf("failed to get state version: %s", err))
+		return
+	}
+
+	state, err = client.StateVersions.Download(ctx, ver.DownloadURL)
+	if err != nil {
+		diag.AddError("tfe client", fmt.Sprintf("failed to download state: %s", err))
+		return
+	}
+
+	return
+}
+
+func getS3ObjectContents(ctx context.Context, client *s3.Client, bucket string, key string) (contents []byte, diag diag.Diagnostics) {
+	resp, err := client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		diag.AddError("s3 client", fmt.Sprintf("failed to get object: %s", err))
+		return
+	}
+	defer resp.Body.Close()
+
+	contents, err = io.ReadAll(resp.Body)
+	if err != nil {
+		diag.AddError("s3 client", fmt.Sprintf("failed to read body: %s", err))
+		return
+	}
+
+	return
+}
+
+func putS3ObjectContents(ctx context.Context, client *s3.Client, bucket string, key string, contents []byte) (diag diag.Diagnostics) {
+	_, err := client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   io.NopCloser(bytes.NewReader(contents)),
+	})
+	if err != nil {
+		diag.AddError("s3 client", fmt.Sprintf("failed s3 put object: %s", err))
+		return
+	}
+
+	return
+}
+
+func validateS3ObjectResource(r *S3ObjectResource) (diag diag.Diagnostics) {
+	if r == nil {
+		diag.AddError("provider", "nil receiver")
+		return
+	}
+
+	if r.s3Client == nil {
+		diag.AddError("provider", "nil s3 client")
+		return
+	}
+
+	if r.tfeClient == nil {
+		diag.AddError("provider", "nil tfe client")
+		return
+	}
+
+	return
+}
+
+func deleteS3Object(ctx context.Context, client *s3.Client, bucket string, key string) (diag diag.Diagnostics) {
+	_, err := client.DeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		diag.AddError("s3 client", fmt.Sprintf("failed to delete s3 object: %s", err))
+		return
+	}
+
+	return
+}
